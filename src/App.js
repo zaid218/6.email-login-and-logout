f24c3212c43f9b3ba53592e2deebc4064d5ba983
Mohd ZAID KHAN <zaidk9620@gmail.com>
2023-07-25 07:59:47 +0530

    useReducer in password and email

--- src/App.js
+++ src/App.js
@@ -5,27 +5,25 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
+
   useEffect(() => {
-    const storedUserLoggedInInformation = localStorage.getItem('is')
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
     if (storedUserLoggedInInformation === '1') {
-      setIsLoggedIn(true)
+      setIsLoggedIn(true);
     }
-  }, [])  
+  }, []);
+
   const loginHandler = (email, password) => {
-    
-      
     // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    localStorage.setItem('is','1')
+    // But it's just a dummy/demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn'); // Update localStorage on logout
     setIsLoggedIn(false);
-    localStorage.removeItem('is');
   };
 
   return (
