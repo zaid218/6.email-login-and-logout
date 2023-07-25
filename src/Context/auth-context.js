a13c5f067a8b50537e912120810860501ef80538
Mohd ZAID KHAN <zaidk9620@gmail.com>
2023-07-25 10:42:51 +0530

    makingapp component linear by the use of usecontext' wq
    'wq

---
+++ src/Context/auth-context.js
@@ -0,0 +1,42 @@
+import { createContext, useEffect, useState } from 'react';
+
+const AuthContext = createContext({
+    isLoggedIn: false,
+    onLogout: () => { },
+    // not necessary but this allows VS Code and React to give suggestions
+    // as we type 'on', it will show the 'onLogout' as a suggestion
+    onLogin: (email, password) => { }
+});
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+        if (storedUserLoggedInInformation === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    return (
+        <AuthContext.Provider value={{
+            isLoggedIn: isLoggedIn,
+            onLogout: logoutHandler,
+            onLogin: loginHandler
+        }}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
